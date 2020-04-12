35876cd1aebf4b3d193cab36c8053378c729208f
gruvw <gruvw.dev@gmail.com>
2020-04-12 15:45:59 +0200

    :bookmark: First release to PyPI

---
+++ description.rst
@@ -0,0 +1,32 @@
+PrintBetter
+===========
+
+Description
+-----------
+
+Use PrintBetter to have a nice prefix before printing anything on the
+console output. It also creates a nice log file where you can find
+anything that you have printed during the program execution. Take a look
+at the `GitHub repository`_ to learn a bit more or to see the
+documentation.
+
+Default example:
+----------------
+
+.. code:: python
+
+   import printbetter as pb
+
+   pb.init()
+
+   pb.info("information")
+   pb.debug("variable debug")
+   pb.warn("warning")
+   pb.err("error")
+
+   pb.exit()
+
+This module was developed by `@Gruvw`_.
+
+.. _GitHub repository: https://github.com/gruvw/printbetter/
+.. _@Gruvw: https://github.com/gruvw
--- setup.py
+++ setup.py
@@ -1,27 +1,30 @@
 from distutils.core import setup
+
 setup(
-  name = 'printbetter',         # How you named your package folder (MyLib)
-  packages = ['printbetter'],   # Chose the same as "name"
-  version = '1.1',      # Start with a small number and increase it with every change you make
-  license='GNU General Public License v3 (GPLv3)',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Use PrintBetter to have a nice prefix before printing anything on the console output. It also creates a nice log file where you can find anything that you have printed during the program execution. Take a look at the GitHub repository where you can also find the documentation: https://github.com/gruvw/printbetter',   # Give a short description about your library
-  author = 'Gruvw',                   # Type in your name
-  author_email = 'gruvw.dev@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/gruvw',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/gruvw/printbetter/archive/v_1.1.tar.gz',    # I explain this later on
-  keywords = ['print', 'log', 'logging', 'time', 'console', 'better', 'printbetter', 'out', 'file', 'logs'],   # Keywords that define your package best
-  install_requires=[],
-  classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-  ],
+    name = 'printbetter',         # How you named your package folder (MyLib)
+    packages = ['printbetter'],   # Chose the same as "name"
+    version = '1.4',      # Start with a small number and increase it with every change you make
+    license='GPLv3',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description = 'Printing perfix and log file!',   # Give a short description about your library
+    long_description=open('description.rst', encoding="UTF-8").read(),
+    author = 'Gruvw',                   # Type in your name
+    author_email = 'gruvw.dev@gmail.com',      # Type in your E-Mail
+    url = 'https://github.com/gruvw/printbetter',   # Provide either the link to your github or to your website
+    download_url = 'https://github.com/gruvw/printbetter/archive/v_1.4.tar.gz',    # I explain this later on
+    keywords = ['print', 'log', 'logging', 'time', 'console', 'better', 'printbetter', 'out', 'file', 'logs'],   # Keywords that define your package best
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   # Again, pick a license
+        'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
 )
